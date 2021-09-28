c295a19a54f57e5f53ed4a511992113ee8b26d73
tlsrhkddus96 <tlsrhkddus96@users.noreply.github.com>
2021-09-28 20:34:31 +0900

    Controller, View

---
+++ src/main/java/org/rhkddus/board/controller/BoardController.java
@@ -0,0 +1,99 @@
+package org.rhkddus.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.rhkddus.board.dto.BoardDTO;
+import org.rhkddus.board.dto.PageRequestDTO;
+import org.rhkddus.board.service.BoardService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@Log4j2
+@RequestMapping("/board/")
+@RequiredArgsConstructor
+public class BoardController {
+
+    private final BoardService boardService;
+
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+
+        log.info("list........"+ pageRequestDTO);
+
+        model.addAttribute("result", boardService.getList(pageRequestDTO));
+
+    }
+
+    @GetMapping("/register")
+    public void register(){
+        log.info("register");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(BoardDTO dto, RedirectAttributes redirectAttributes){
+
+        log.info("dto...."+ dto);
+
+        Long bno = boardService.register(dto);
+
+        log.info("BNO : " + bno);
+
+        redirectAttributes.addFlashAttribute("msg", bno);
+
+        return "redirect:/board/list";
+
+    }
+
+    @GetMapping({"/read","/modify"})
+    public void read(@ModelAttribute("requestDTO") PageRequestDTO pageRequestDTO, Long bno, Model model){
+
+        log.info("bno:" + bno);
+
+        BoardDTO boardDTO = boardService.get(bno);
+
+        log.info(boardDTO);
+
+        model.addAttribute("dto", boardDTO);
+
+    }
+
+    @PostMapping("/remove")
+    public String remove(long bno, RedirectAttributes redirectAttributes){
+
+        log.info("bno: " + bno);
+
+        boardService.removeWithReplies(bno);
+
+        redirectAttributes.addFlashAttribute("msg", bno);
+
+        return "redirect:/board/list";
+
+
+    }
+
+    @PostMapping("/modify")
+    public String modify(BoardDTO dto,@ModelAttribute("requestDTO") PageRequestDTO requestDTO, RedirectAttributes redirectAttributes){
+
+        log.info("POST Modify");
+        log.info("dto : " + dto);
+
+        boardService.modify(dto);
+
+        redirectAttributes.addFlashAttribute("page", requestDTO.getPage());
+        redirectAttributes.addFlashAttribute("type", requestDTO.getType());
+        redirectAttributes.addFlashAttribute("keyword", requestDTO.getKeyword());
+        redirectAttributes.addFlashAttribute("bno", dto.getBno());
+
+        return "redirect:/board/read";
+
+    }
+
+
+}
